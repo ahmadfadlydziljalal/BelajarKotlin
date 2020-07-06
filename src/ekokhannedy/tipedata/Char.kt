264323eb08e001b6e3b8dde732b5fe7dc042f082
dziljalal1 <dziljalal1@gmail.com>
2020-07-06 15:37:24 +0700

    Belajar Tipe Data Character

---
+++ src/ekokhannedy/tipedata/Char.kt
@@ -0,0 +1,16 @@
+package ekokhannedy.tipedata
+
+fun main(){
+    var title : String = "Char diapit dengan tanda petik satu"
+    var char1 : Char = 'D'
+    var char2 : Char = 'Z'
+    var char3 : Char = 'I'
+    var char4 : Char = 'L'
+
+
+    println(title)
+    print(char1)
+    print(char2)
+    print(char3)
+    print(char4)
+}
