f4b430e4a1609d9ef367848b45f06a3072a7e0cd
dziljalal1 <dziljalal1@gmail.com>
2020-07-07 13:25:01 +0700

    Operasi Perbandingan Pada Kotlin

---
+++ src/ekokhannedy/operasi/perbandingan/Perbadingan.kt
@@ -0,0 +1,17 @@
+package ekokhannedy.operasi.perbandingan
+
+fun main() {
+    val a = 100
+    val b = 200
+
+    println(a < b)
+    println(a > b)
+
+    val result : Boolean = a < b
+    println(result)
+
+    println("a" < "b")
+    println("a" > "b")
+    println("a" == "a")
+    println("a" != "a")
+}
---
+++ src/ekokhannedy/operasi/perbandingan/README.md
@@ -0,0 +1,10 @@
+# Pebandingan dan Boolean
+
+Operator yang biasa digunakan
+
+    1. > Lebih besar
+    2. < Lebih kecil
+    3. <= Lebih kecil sama dengan
+    4. >= Lebih besar sama dengan 
+    5 == Sama dengan
+    6.!= Sama dengan 
